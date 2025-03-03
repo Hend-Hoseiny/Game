77636bf3e1b4ec4d3407fd4c26e55800b3ad62ab
Hend Hoseiny <hendshahhat9@gmail.com>
2025-03-04 00:26:20 +0200

    Reapply "Add files via upload"
    This reverts commit 2a3036f6193948bc2e130ddb43c643f1f068efc3.

---
+++ game/src/engine/board/Cell.java
@@ -0,0 +1,40 @@
+package engine.board;
+
+public class Cell {
+ //  A class representing the Cells available on the board.
+	private Marble marble; // all attributes are read and write
+	private CellType cellType;
+	private boolean trap;
+	
+	public Cell(CellType cellType){
+		this.cellType = cellType;
+		this.marble = null;
+		this.trap = false;
+	}
+
+	public Marble getMarble() {
+		return marble;
+	}
+
+	public void setMarble(Marble marble) {
+		this.marble = marble;
+	}
+
+	public CellType getCellType() {
+		return cellType;
+	}
+
+	public void setCellType(CellType cellType) {
+		this.cellType = cellType;
+	}
+
+	public boolean isTrap() {
+		return trap;
+	}
+
+	public void setTrap(boolean trap) {
+		this.trap = trap;
+	}
+	
+	
+}
