2a3036f6193948bc2e130ddb43c643f1f068efc3
Hend Hoseiny <hendshahhat9@gmail.com>
2025-03-04 00:15:45 +0200

    Revert "Add files via upload"
    This reverts commit ca8fd59f9faf7959180b3010e026ef244d3ec355.

--- game/src/model/card/wild/Wild.java
+++
@@ -1,9 +0,0 @@
-package model.card.wild;
-
-abstract class Wild extends Card  {
- // A class representing the wild cards available in the game.
-//A wild card has the following 2 subclasses: Burner, Saver
-	public Wild(String name, String description, BoardManager boardManager, GameManager gameManager){
-		super(name,description, boardManager,gameManager);
-	}
-}
