1827cf8454254319d25143ec823e0300e9c94bd4
xxjessiexx <yasmin.khaled.aly@gmail.com>
2025-04-19 23:08:57 +0200

    Update Saver.java

--- src/model/card/wild/Saver.java
+++ src/model/card/wild/Saver.java
@@ -7,5 +7,21 @@ public class Saver extends Wild {
     public Saver(String name, String description, BoardManager boardManager, GameManager gameManager) {
         super(name, description, boardManager, gameManager);
     }
+    
+    @Override
+    public void act(ArrayList<Marble> marbles) throws ActionException, InvalidMarbleException {
+        if (!validateMarbleSize(marbles)) {
+            throw new InvalidMarbleException("Saver requires exactly 1 marble");
+        }
+        if (!validateMarbleColours(marbles)) {
+            throw new InvalidMarbleException("Saver can only save your marbles");
+        }
+
+        try {
+            boardManager.sendToSafe(marbles.get(0)); 
+        } catch (InvalidMarbleException e) {
+            throw new ActionException("Save failed: " + e.getMessage(), e);
+        }
+    }
 
 }
