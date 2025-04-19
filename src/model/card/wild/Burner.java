ea94f871f4e5135dfd93df3d32b1f2504854c8a8
xxjessiexx <yasmin.khaled.aly@gmail.com>
2025-04-19 23:08:10 +0200

    Update Burner.java

--- src/model/card/wild/Burner.java
+++ src/model/card/wild/Burner.java
@@ -7,5 +7,35 @@ public class Burner extends Wild {
     public Burner(String name, String description, BoardManager boardManager, GameManager gameManager) {
         super(name, description, boardManager, gameManager);
     }
+    @Override
+    public boolean validateMarbleColours(ArrayList<Marble> marbles){
+    	Colour playerColor = GameManager.getActivePlayerColour(); 
+    	if(marbles.size() == 1){
+    	Colour givenColor = marbles.get(0).getColor();
+    	if (!playerColor.equals(givenColor){
+    		return true;
+    	}}
+    	
+    	else{
+    		return false;
+    	}
+    }
+    
+    @Override
+    public void act(ArrayList<Marble> marbles) throws ActionException,InvalidMarbleException {
+        try {
+            if (!validateMarbleSize(marbles)) {
+                throw new ActionException("Burner requires exactly 1 marble");
+            }
+            if (!validateMarbleColours(marbles)) {
+                throw new ActionException("Burner can only target opponent marbles");
+            }
+            
+            boardManager.destroyMarble(marbles.get(0));
+        } catch (InvalidMarbleException | IllegalDestroyException e) {
+            throw new ActionException("Burn failed: " + e.getMessage(), e);
+        }
+    }
+
 
 }
