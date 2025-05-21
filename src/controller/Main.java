126ea3333c6db83aa4f56543e6379313d772e645
Hend Hoseiny <hendshahhat9@gmail.com>
2025-05-21 21:49:28 +0300

    more changes

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -36,46 +36,15 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // myView v = new myView();
-        // myController c = new myController(primaryStage, v, v.getMainScene());
-        // c.setControls();
-
-        // primaryStage.setScene(v.getStartScene());
-        // primaryStage.setResizable(false);
-        // primaryStage.setFullScreen(false);
-        // primaryStage.show();
-
-        Deck.loadCardPool(null, null);
-        ArrayList<Card> c = Deck.getDeck();
-        System.out.println(c);
-        // StackPane s = new StackPane();
-        // ImageView v = new ImageView(new Image(getCardURL(c.get(0))));
-        // s.getChildren().add(v);
-        // Scene sc = new Scene(s,1000,600);
-        // primaryStage.setScene(sc);
-        // primaryStage.show();
-
+        myView v = new myView();
+        myController c = new myController(primaryStage, v, v.getMainScene());
+        c.setControls();
+        
+        primaryStage.setScene(v.getStartScene());
+        primaryStage.setResizable(false);
+        primaryStage.setFullScreen(false);
+        primaryStage.show();
     }
 
-    private String getCardURL(Card c) {
-        if (c instanceof Standard) {
-            int rank = ((Standard) c).getRank();
-            String suit = ((Standard) c).getSuit().toString();
-            suit.toLowerCase();
-            suit += "s";
-            if (rank == 11)
-                return "file:resources/images/jack of " + suit + ".png";
-            if (rank == 12)
-                return "file:resources/images/queen of " + suit + ".png";
-            if (rank == 13)
-                return "file:resources/images/king of " + suit + ".png";
-            return "file:resources/images/" + rank + " of " + suit + ".png";
-        } else if (c instanceof Burner) {
-            return "file:resources/images/burner.png";
-        } else if (c instanceof Saver) {
-            return "file:resources/images/saver.png";
-        } else
-            return ("");
-    }
-
+    
 }
